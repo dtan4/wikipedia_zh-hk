bccba4fb0e630728f9cd66111278cc2dab083f5f
dtan4 <dtanshi45@gmail.com>
2013-11-10 03:10:08 +0900

    Use window.location.href not to remain history

--- background.js
+++ background.js
@@ -1,10 +1,8 @@
 chrome.tabs.onUpdated.addListener(function(tabId, info, tab) {
     var url = tab.url;
 
-    if (url.match(/https?:\/\/zh\.wikipedia\.org\/wiki\/.+/)) {
+    if (info.status == "loading" && url.match(/https?:\/\/zh\.wikipedia\.org\/wiki\/.+/)) {
         var hk_url = url.replace(/(^https?:\/\/zh\.wikipedia\.org)\/wiki\/(.+)$/, "$1/zh-hk/$2");
-        chrome.tabs.update(tabId, {"url" : hk_url}, function(tab) {
-            console.log("redirected to " + hk_url);
-        });
+        chrome.tabs.executeScript(tabId, {"code" : "window.location.href = '" + hk_url + "';"});
     }
 });
